e27e5273bbf85cf411a2e43b0028e70bad5cad4e
PAOLA GUZMAN <santyalfg@gmail.com>
2025-11-12 11:31:12 -0500

    Version mejorada de comentarios caso #9

--- test/login/test_login_admin_no_creado.py
+++ test/login/test_login_admin_no_creado.py
@@ -1,7 +1,8 @@
+
 # Fecha creacion: 10/11/2025
-# Ultima fecha de modificación: 12/11/2025
+# Ultima fecha de modificación: 10/11/2025
 # Autor: David Santiago Alfonso Guzman
-# Descripcion: Este archivo prueba el test case #8 sobre el incio de sesion administrador datos equivocados  
+# Descripcion: Este archivo prueba el test case #9 sobre el Inicio de sesion administrador datos no creado
 
 import pytest
 from pages.login_page import LoginPage
@@ -27,31 +28,3 @@ def test_inicio_sesion_admin_no_creado(setup):
 
     pass
 
-"""
-    CASO DE PRUEBA #12
-    Nombre: Inicio de sesion administrador datos no creado
-
-    Descripción:
-        El sistema debe validar y  NO dar acceso a un usuario tipo administrador con sus datos 
-        equivocados, y debera mostrara la notificacion de inicio en sesión
-
-    Precondiciones:
-        - El usuario no está registrado en el sistema.
-
-    Entradas:
-        - Username (usuario no creado)
-        - Contraseña (cualquiera)
-
-    Pasos:
-        1. Dar click en el botón "Inicio de sesión".
-        2. Llenar con los datos del usuario no registrado.
-        3. Dar click en "Ingresar".
-
-    Resultados esperados:
-        1. El sistema no permite ingresar al dashboard.
-        2. Muestra una notificación indicando "usuario no registrado" o similar.
-        3. No se muestran alertas por falta de datos.
-    
-    Poscondición:
-        El sistema permanece en la pantalla de inicio de sesión.
-    """
